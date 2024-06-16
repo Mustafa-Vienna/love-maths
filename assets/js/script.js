82aa7c148bfc4385c7f309cfac78d30e51d45e68
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-06-16 09:27:18 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrong() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
